82696cb401adb586dd69c54674a20cd6f35000a8
Hammadsoloman <hammadali95@yahoo.com>
2020-09-16 16:41:01 +0300

    finish context and work now on pull it in th componants

--- expense-tracker/src/components/TransactionList.js
+++ expense-tracker/src/components/TransactionList.js
@@ -1,14 +1,23 @@
-import React from 'react'
+import React,{useContext} from 'react'
+import {Transaction} from './Transaction'
+
+
+import { GlobalContext } from '../context/GlobaleState'
 
 export const TransactionList = () => {
-    return (
-        <>
-             <h3>History</h3>
-      <ul  className="list">
-        <li className="minus">
-          Cash <span>-$400</span><button className="delete-btn">x</button>
-        </li> 
+
+  const {transactions} = useContext(GlobalContext);
+  
+  // console.log('transactions',transactions)
+
+  return (
+    <>
+      <h3>History</h3>
+      <ul className="list">
+          {transactions.map(transaction=>(<Transaction key={transaction.id} transaction={transaction}  /> ))}
+        
       </ul>
-        </>
-    )
+    </>
+
+  )
 }
