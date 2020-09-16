6e9c68afeb7de693655e7ef9a816701da95bf0b9
Hammadsoloman <hammadali95@yahoo.com>
2020-09-16 18:56:51 +0300

    finish the front-end side of the app

--- expense-tracker/src/components/addTransaction.js
+++ expense-tracker/src/components/addTransaction.js
@@ -1,17 +1,34 @@
-import React, {useState} from 'react'
+import React, { useState, useContext } from 'react'
+import { GlobalContext } from '../context/GlobaleState'
 
 export const AddTransaction = () => {
+
+  const { addTransaction } = useContext(GlobalContext);
+
   const [text,setText]=useState('');
   const [amount, setAmount]=useState(0)
+
+  const onSubmit = e => {
+    e.preventDefault();
+
+    const newTransaction = {
+      id: Math.floor(Math.random() * 100000000),
+      text,
+      // this because we faced the number that we add being as a string, so this oe of the way ro parse the string.
+      amount: +amount
+    }
+
+    addTransaction(newTransaction);
+  }
 
 
     return (
         <div>
             <h3>Add new transaction</h3>
-      <form >
+      <form onSubmit={onSubmit} >
         <div className="form-control">
           <label for="text">Text</label>
-          <input type="text" value={text} onChange={(e)=> setText(e.target.value)} placeholder="Enter text..." />
+          <input type="text" value={text} onChange={(e)=> setText(e.target.value)}  placeholder="Enter text..." />
         </div>
         <div className="form-control">
           <label htmlFor="amount"
