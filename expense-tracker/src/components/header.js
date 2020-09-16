e4a9320a3ce8978bc6819f2af1350167a644eacd
Hammadsoloman <hammadali95@yahoo.com>
2020-09-16 12:08:19 +0300

    add App.css

---
+++ expense-tracker/src/components/header.js
@@ -0,0 +1,19 @@
+// import React from 'react'
+
+// export const header = () => {
+//     return (
+//         <div>
+            
+//         </div>
+//     )
+// }
+
+import React from 'react';
+
+export const Header =()=>{
+    return (
+        <div>
+          Expense Tracker  
+        </div>
+    )
+}
