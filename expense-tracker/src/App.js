b771b54252db310070ac1fc7f27a4284d0c1af5f
Hammadsoloman <hammadali95@yahoo.com>
2020-09-16 12:44:56 +0300

    add Balance Header Income components

--- expense-tracker/src/App.js
+++ expense-tracker/src/App.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import './App.css';
 import {Header} from './components/header';
+import {Balance} from './components/Balance'
+import {IncomeExpenses} from './components/IncomeExpenses'
+
 
 function App() {
   return (
     <div>
      <Header />
+     <div className='container' >
+       <Balance/>
+       <IncomeExpenses/>
+     </div>
     </div>
   );
 }
