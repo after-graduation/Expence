617f5abd7829c4c1971c529924918b83a370b472
Hammadsoloman <hammadali95@yahoo.com>
2020-09-17 19:08:32 +0300

    setup the routes

--- controller/transaction.js
+++ controller/transaction.js
@@ -5,3 +5,19 @@
 exports.getTransactions=(req,res,next)=>{
     res.send('GET transactions')
 }
+
+// @desc add all transactions
+// POST /api/v1/transactions
+
+
+exports.addTransactions=(req,res,next)=>{
+    res.send('delete transactions')
+}
+
+// @desc delete all transactions
+// delete /api/v1/transactions/:id
+
+
+exports.deleteTransactions=(req,res,next)=>{
+    res.send('delete transactions')
+}
