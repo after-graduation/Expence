ffcef18c7bbb00a19b86f78112cae1282cf91ef5
Hammadsoloman <hammadali95@yahoo.com>
2020-09-21 10:29:14 +0300

    try new pattern to the controller

--- controller/transaction.js
+++ controller/transaction.js
@@ -1,23 +1,73 @@
-// @desc Get all transactions
-// GET /api/v1/transactions
+const Transaction = require('../models/Transaction');
 
+exports.getTransactions = async (req, res, next) => {
+  try {
+    const transactions = await Transaction.find();
 
-exports.getTransactions=(req,res,next)=>{
-    res.send('GET transactions')
+    return res.status(200).json({
+      success: true,
+      count: transactions.length,
+      data: transactions
+    });
+  } catch (err) {
+    return res.status(500).json({
+      success: false,
+      error: 'Server Error'
+    });
+  }
 }
 
-// @desc add all transactions
-// POST /api/v1/transactions
 
+exports.addTransaction = async (req, res, next) => {
+  try {
+    const { text, amount } = req.body;
 
-exports.addTransactions=(req,res,next)=>{
-    res.send('delete transactions')
+    const transaction = await Transaction.create(req.body);
+  
+    return res.status(201).json({
+      success: true,
+      data: transaction
+    }); 
+  } catch (err) {
+    if(err.name === 'ValidationError') {
+      const messages = Object.values(err.errors).map(val => val.message);
+
+      return res.status(400).json({
+        success: false,
+        error: messages
+      });
+    } else {
+      return res.status(500).json({
+        success: false,
+        error: 'Server Error'
+      });
+    }
+  }
 }
 
-// @desc delete all transactions
-// delete /api/v1/transactions/:id
 
+exports.deleteTransaction = async (req, res, next) => {
+  try {
+    const transaction = await Transaction.findById(req.params.id);
 
-exports.deleteTransactions=(req,res,next)=>{
-    res.send('delete transactions')
+    if(!transaction) {
+      return res.status(404).json({
+        success: false,
+        error: 'No transaction found'
+      });
+    }
+
+    await transaction.remove();
+
+    return res.status(200).json({
+      success: true,
+      data: {}
+    });
+
+  } catch (err) {
+    return res.status(500).json({
+      success: false,
+      error: 'Server Error'
+    });
+  }
 }
