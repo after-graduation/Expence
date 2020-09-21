b3c794068423dfef089ab3e7b984feb49f9c24ec
Hammadsoloman <hammadali95@yahoo.com>
2020-09-21 10:26:27 +0300

    server.js

--- server.js
+++ server.js
@@ -1,13 +1,32 @@
-const express=require('express');
-const dotenv =require('dotenv');
+const path = require('path');
+const express = require('express');
+const dotenv = require('dotenv');
 const colors = require('colors');
-const morgan= require('morgan');
+const morgan = require('morgan');
+const connectDB = require('./config/db');
 
-dotenv.config({path:'./config/config.env'})
-const transaction = require('./routes/transaction')
-const app=express();
+dotenv.config({ path: './config/config.env' });
 
-app.use('/api/v1/transaction',transaction)
+connectDB();
+
+const transactions = require('./routes/transactions');
+
+const app = express();
+
+app.use(express.json());
+
+if(process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
+
+app.use('/api/v1/transactions', transactions);
+
+if(process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')));
+}
+
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, console.log(`server running in ${PORT}`))
+app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold));
